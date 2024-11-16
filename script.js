31533dd0879efba3b17ef580eb28e7a5b1351b54
16bitdevelopment <16bitdevelopment@gmail.com>
2024-11-16 11:35:39 +1100

    Nav done

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Cursor
+
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (event) => {
+    const {clientX, clientY} = event;
+
+    cursor.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, {duration: 3000, fill: "forwards"});
+});
+
 // Scroll animations
 
 window.addEventListener("scroll", setScrollVar);
